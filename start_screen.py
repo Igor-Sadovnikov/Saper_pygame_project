49f945a5fde369f9b6fc91899737dfc92fcecb48
Samira1307 <faizova1307@gmail.com>
2025-01-21 18:54:10 +0300

    Add files via upload

--- start_screen.py
+++ start_screen.py
@@ -1,24 +1,17 @@
-FPS = 50
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
 def start_screen():
     intro_text = ["САПЕР"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('fon.png'), (600, 600))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    font = pygame.font.SysFont('serif', 72) 
+    text_coord = 120
+    color = (255, 255, 255)
+    rect_position = [205, 390, 200, 50]
+    pygame.draw.rect(screen, color , rect_position)
+    pygame.font.init()
+    my_font_0 = pygame.font.SysFont('Classy Vogue', 50)
+    text_surface_restart = my_font_0.render('Новая игра', False, (0, 0, 0))
+    text_surface_restart_rect = text_surface_restart.get_rect(topleft=(210, 400))
+    board.screen.blit(text_surface_restart, text_surface_restart_rect)
 
     clock = pygame.time.Clock()
 
@@ -26,8 +19,8 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if text_surface_restart_rect.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN:
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
